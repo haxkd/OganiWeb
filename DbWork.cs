3a2e9879aacb2c8d96c57fa76d057f41e6855748
Abul Hasan <33129246+haxkd@users.noreply.github.com>
2023-05-20 15:22:15 +0530

    '20/05/2023'

---
+++ DbWork.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OganiWeb
+{
+    public class DbWork
+    {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString);
+        public SqlDataReader reader;
+        public SqlDataReader getAllProducts()
+        {
+            connection.Open();
+            return new SqlCommand("SELECT * FROM Product", connection).ExecuteReader();
+        }
+
+    }
+}
